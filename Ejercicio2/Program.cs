55b6566a7249e31e23c8e80202b5a485226d4eef
RuiJ72 <rlagos900@gmail.com>
2022-10-29 18:24:34 -0300

    Ejercicio 2

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("caracteristicas de un Coche");
+Console.WriteLine("____________________________");
+int puertasNumero = 4;
+int ruedasNumero = 4;
+string marca = "Seat";
+float licencia = 2022.07f;
+
+Console.WriteLine("Numero de Puertas: " + Convert.ToString(puertasNumero));
+Console.WriteLine("Numero de Ruedas: " + Convert.ToString(ruedasNumero));
+Console.WriteLine("Marca: " + Convert.ToString(marca));
+Console.WriteLine("Permiso para circular nr: " + Convert.ToString(licencia));
+Console.WriteLine("\n");
+
+
+
+Console.WriteLine("Caracteristicas de una mesa: ");
+Console.WriteLine("_____________________________");
+float PesoKg = 45.60f;
+int largoCm = 120;
+string Material = "Madera";
+string Color = "Blanca";
+
+
+Console.WriteLine("Peso en Kg: " + Convert.ToString(PesoKg));
+Console.WriteLine("largo de la mesa en cm: " + Convert.ToString(largoCm));
+Console.WriteLine("Material de construcción " + Convert.ToString(Material));
+Console.WriteLine("Color: " + Color);
+
