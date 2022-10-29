05fc51a2d279df47aede38b62735dc32fd263ecf
RuiJ72 <rlagos900@gmail.com>
2022-10-29 18:45:35 -0300

    Ejercicio 3

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+int number = 18;
+
+char caracterA = 'a';
+char vIn = '\xa';
+bool vOut = (vIn != 0);
+
+
+
+Console.WriteLine("Condition True: Número mayor que 18");
+Console.WriteLine("---------------------------");
+Console.WriteLine(18 >= 18);
+
+Console.WriteLine("Condition True: a es un char");
+Console.WriteLine("---------------------------");
+Console.WriteLine(vIn);
+
+
+Console.WriteLine("Dos condiciones verdaderas");
+Console.WriteLine("----------------------------");
+Console.WriteLine(18 == 18 && 17 <= 18);
+
+Console.WriteLine("----------------------------");
+Console.WriteLine("Una de dos condiciones verdadera");
+Console.WriteLine("----------------------------");
+
+Console.WriteLine(19 >= 18 || 65 == 65);
+
